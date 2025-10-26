0e310439b19e601df4e13b3875cd2a6cc08764ae
Tutorbin <tutorbin@Tutorbins-MacBook-Pro.local>
2025-10-26 22:28:15 +0530

    fix: handling post method logic in products api.

--- handlers/product.go
+++ handlers/product.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -17,6 +20,18 @@ func (ph *ProductHandler) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
 		ph.getProducts(rw)
 		return
 	}
+	if h.Method == "POST" {
+		body, err := io.ReadAll(h.Body)
+		if err != nil {
+			http.Error(rw, "Malformed request body", http.StatusBadRequest)
+		}
+		var product schemas.Product
+		json.Unmarshal(body, &product)
+		fmt.Println("Product unmarshalled is", product)
+		ph.addProduct(&product)
+		log.Printf("Product added successfully")
+		return
+	}
 	// fmt.Println("Hello product")
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 
@@ -30,4 +45,8 @@ func (ph *ProductHandler) getProducts(rw http.ResponseWriter) {
 		http.Error(rw, "Some json encoding error", http.StatusInternalServerError)
 		return
 	}
+}
+func (ph *ProductHandler) addProduct(product *schemas.Product) {
+	schemas.ProductList = append(schemas.ProductList, product)
+	fmt.Println("List is", schemas.ProductList)
 }
